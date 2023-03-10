629f8a642940a11a56557c55b2a5454aad4a3788
cvalenzuela1 <101999993+cvalenzuela1@users.noreply.github.com>
2023-03-09 21:54:32 -0300

    Obtener Pais local

--- pipesApp/src/app/ventas/pages/comunes/comunes.component.ts
+++ pipesApp/src/app/ventas/pages/comunes/comunes.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-comunes',
   templateUrl: './comunes.component.html',
   styleUrls: ['./comunes.component.css']
 })
-export class ComunesComponent {
+export class ComunesComponent implements OnInit{
+  
   nombreLowercase: string = "camilo valenzuela";
   nombreUppercase: string = "CAMILO VALENZUELA";
   nombreCompleto: string = "CaMiLo VaLeNzUeLa";
+
+  countries: object = {
+    esCL: "Chile",
+    esAR: "Argentina"
+  };
+
+  fecha: Date = new Date();
+  fecha_local: string = this.fecha.toLocaleDateString();
+
+  locale: string = "";
+
+  ngOnInit(): void {
+    this.getCountryLocale();
+  }
+
+  getCountryLocale(): void {
+    const locale_value = Intl.DateTimeFormat().resolvedOptions().locale;
+    for (const valor of Object.entries(this.countries)) {  
+      for (let i = 0; i < valor.length; i++) {
+        const element = valor[0];
+        if(i == 1 && locale_value.replace('-','') === element) {
+          this.locale = valor[1];
+          console.log(this.locale);
+          break;
+        }
+      }
+    }
+  }
+  
 }
